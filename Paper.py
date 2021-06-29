1c20db6e2d19abfe05ef879f99ccddee5d967046
Payam Saeedi <76733438+payamv3@users.noreply.github.com>
2021-06-30 02:28:02 +0430

    Update Paper.py

--- Paper.py
+++ Paper.py
@@ -8,19 +8,7 @@ import sys
 import time
 from bs4 import BeautifulSoup
 
-paper = pd.DataFrame()
-
-for page_no in range(1, 253):
-    
-    data = {
-        'page': page_no,
-        'size': 50
-    }
-    page = requests.get('https://paperds.ir/book-publisher-report', params=data, verify = False)
-    soup = BeautifulSoup(page.text, 'html.parser')   
-    table = soup.find_all('table')
-    df = pd.read_html(str(table))[0]
-    paper = paper.append(df)
+paper = pd.read_excel(PaperSystem.xlsx')
     
 st.write("""
 # Allocation of Paper to Publishers in Iran
